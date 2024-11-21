efdaab2363b16c9e2769924526b2c65e9d050d9f
its-aadi7 <dhuladev27@gmail.com>
2024-11-21 16:23:43 +0530

    practice

---
+++ 01_functions.py
@@ -0,0 +1,27 @@
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
+
+# average = (a + b + c)/3
+# print(average)
+
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
+
+# average = (a + b + c)/3
+# print(average)
+
+def avg():
+    a = int(input("Enter your number: "))
+    b = int(input("Enter your number: "))
+    c = int(input("Enter your number: "))
+
+    average = (a + b + c)/3
+    print(average)
+
+avg()
+avg()
+avg()
+avg()
+avg()
