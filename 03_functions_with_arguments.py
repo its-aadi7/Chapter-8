707e73124c90341010f452a0ccf78a1bf6b43678
its-aadi7 <dhuladev27@gmail.com>
2024-11-28 16:29:52 +0530

    new

--- 03_functions_with_arguments.py
+++ 03_functions_with_arguments.py
@@ -1,8 +1,7 @@
 def goodDay(name, ending):
     print("Good Day, " + name)
     print(ending)
+    return "OK"
 
-goodDay("Aadi", "Thank you")
-goodDay("Jash", "Thank you")
-goodDay("Nilay", "Thank you")
-goodDay("Krutika", "Thanks")
+a = goodDay("Aadi", "Thank you")
+print(a)
